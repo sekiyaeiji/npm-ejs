06ffc2385f27f2d3183656c77ffd76bd55e3856c
関家 栄治 <sekiya@mc24314-bp15-1.gndomain.gnavi.co.jp>
2019-07-22 19:05:42 +0900

    シェルjson操作

--- bin/build_html.sh
+++ bin/build_html.sh
@@ -6,7 +6,8 @@ echo {"stat_version": "${version}"}
 
 RESULT_JSON='src/html/data/data2.json'
 
-cat src/html/data/data.json | jq '.jobanalyses[0] |= .+ {"stat_version": "${version}"}' \
-> ${RESULT_JSON} && cat ${RESULT_JSON}
+data=cat src/html/data/data.json | jq '.jobanalyses[0] |= .+ {"stat_version": "${version}"}' \> ${RESULT_JSON} && cat ${RESULT_JSON}
+
+echo {"stat_version": "${version}"}
 
 ejs-cli -b src/html/page '**/*.ejs' -o dist -O src/html/data/data2.json
